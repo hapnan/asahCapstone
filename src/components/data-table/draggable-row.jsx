0f1bb28cad2a0005ef9623654224a6d8bb8b15ee
TatakAdi <tatakadisubagyo@gmail.com>
2025-11-22 00:20:20 +0800

    refactor: Buat file data-table.jsx menjadi lebih modular biar gampang di maintenance

---
+++ src/components/data-table/draggable-row.jsx
@@ -0,0 +1,30 @@
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import { flexRender } from "@tanstack/react-table";
+
+import { TableRow, TableCell } from "@/components/ui/table";
+
+export function DraggableRow({ row }) {
+  const { transform, transition, setNodeRef, isDragging } = useSortable({
+    id: row.original.id,
+  });
+
+  return (
+    <TableRow
+      data-state={row.getIsSelected() && "selected"}
+      data-dragging={isDragging}
+      ref={setNodeRef}
+      className="relative z-0 data-[dragging=true]:z-10 data-[dragging=true]:opacity-80"
+      style={{
+        transform: CSS.Transform.toString(transform),
+        transition: transition,
+      }}
+    >
+      {row.getVisibleCells().map((cell) => (
+        <TableCell key={cell.id}>
+          {flexRender(cell.column.columnDef.cell, cell.getContext())}
+        </TableCell>
+      ))}
+    </TableRow>
+  );
+}
